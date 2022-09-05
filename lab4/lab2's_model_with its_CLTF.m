79d2e258a864031f9d6c3f54f62a94637e566f60
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-09-05 16:56:43 -0400

    Create lab2's_model_with its_CLTF.m

---
+++ lab4/lab2's_model_with its_CLTF.m
@@ -0,0 +1,13 @@
+time = data(:,2); %read Time
+y=data(:,4); %set Encode1 Pos as y
+u=data(:,3); % Control Position as input
+zf = iddata(y,u,0.00884); %set Object iddata
+Tfd2 = tfest(zf,2,0); % for second order
+plot(time,y);
+
+K=5, B=0.002, J=0.02; %set K, B and J
+Kp = 0.1, Kd = 0.01; %set Kp and Kd
+s = tf('s'); %call transfer function
+cltf = (K*Kp)/(J*s^2+(B+K*Kd)*s + K*Kp);
+
+%get closed loop transfer function when s is defined as a term for transfer function
