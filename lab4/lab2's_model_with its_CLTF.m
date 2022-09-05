bf9ecd19b5ee8684666ff19a9b15a828c791e1ea
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-09-05 16:59:07 -0400

    Update lab2's_model_with its_CLTF.m

--- lab4/lab2's_model_with its_CLTF.m
+++ lab4/lab2's_model_with its_CLTF.m
@@ -8,6 +8,10 @@ plot(time,y);
 K=5, B=0.002, J=0.02; %set K, B and J
 Kp = 0.1, Kd = 0.01; %set Kp and Kd
 s = tf('s'); %call transfer function
-cltf = (K*Kp)/(J*s^2+(B+K*Kd)*s + K*Kp);
+cltf = (K*Kp)/(J*s^2+(B+K*Kd)*s + K*Kp); %get closed loop transfer function when s is defined as a term for transfer function
 
-%get closed loop transfer function when s is defined as a term for transfer function
+ltiview('step',Tfd2, 0:0.1:10)
+% To view Tfd2 with step response where time varies from 0 to 10 with step size of 0.1 second
+ltiview('step',cltf, 0:0.1:10)
+% To view cltf with step response where time varies from 0 to 10 with step size of 0.1 second
+
